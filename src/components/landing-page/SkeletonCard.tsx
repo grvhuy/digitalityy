49c5b74f70f1ade6117d2a79cb6cea0bdd2e0771
Mingu <vhnm3004@outlook.com>
2024-06-24 01:32:01 +0700

    Further refinement to the Landing Page

--- src/components/landing-page/SkeletonCard.tsx
+++ src/components/landing-page/SkeletonCard.tsx
@@ -6,12 +6,11 @@ interface Cards {
   // height: number;
 }
 
-export function SkeletonCard(props: Cards) {
-  return Array(props.length)
-    .fill(0)
-    .map((_, i) => (
-      <div key={i} className="flex rounded-lg">
-        <Skeleton className={`h-[22rem] w-[20rem]`} />
-      </div>
-    ));
+export function SkeletonCard() {
+  return (
+    <div className="space-y-2">
+      <Skeleton className="h-[16rem] w-[16rem]" />
+      <Skeleton className="h-4 w-[16rem]" />
+    </div>
+  );
 }
