ddac44b14526acdc73ec92983f4265739b756391
grvhuy <21522171@gm.uit.edu.vn>
2024-06-29 23:26:53 +0700

    Update SettingsMenu.tsx

--- src/components/settings/SettingsMenu.tsx
+++ src/components/settings/SettingsMenu.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import axios from "axios";
+import Link from "next/link";
 
 export default function SettingsMenu() {
   const { data: session } = useSession();
@@ -61,7 +62,7 @@ export default function SettingsMenu() {
       <div>
         <ul className="font-medium hover:[&>li]:scale-[.99] [&>li]:p-4 hover:[&>li]:bg-eerie_black hover:[&>li]:text-yellow-400 [&>li]:transition-all [&>li]:duration-200 [&>li]:rounded-lg [&>li]:cursor-pointer [&>li]:flex [&>li]:flex-row [&>li]:relative">
           <li className="flex flex-row relative group " onClick={handleEdit}>
-            Edit Profile
+            Profile
             <MdOutlineKeyboardArrowRight
               size={25}
               className="absolute right-0 text-gray-400"
@@ -69,7 +70,7 @@ export default function SettingsMenu() {
           </li>
           <Separator />
           <li>
-            Order History
+            <Link href={`/user/orders`}>Order History</Link>
             <MdOutlineKeyboardArrowRight
               size={25}
               className="absolute right-0 text-gray-400"
@@ -77,13 +78,13 @@ export default function SettingsMenu() {
           </li>
           <Separator />
           <li onClick={handleAddresses}>
-            Manage Addresses{" "}
+            <Link href={`/user/settings/addresses`}>Addresses</Link>
             <MdOutlineKeyboardArrowRight
               size={25}
               className="absolute right-0 text-gray-400"
             />
           </li>
-          <Separator />
+          {/* <Separator />
           <li onClick={handleChangePassword}>
             Change password{" "}
             <MdOutlineKeyboardArrowRight
@@ -91,7 +92,7 @@ export default function SettingsMenu() {
               className="absolute right-0 text-gray-400"
             />
           </li>
-          <Separator />
+          <Separator /> */}
           <Separator />
         </ul>
       </div>
